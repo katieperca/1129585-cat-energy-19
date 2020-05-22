40ca4b382e474f2525e26a656cff306739c52233
katieperca <katieperca@gmail.com>
2020-05-22 16:54:26 +0300

    Изменяет координаты метки на карте на десктопе

--- source/js/script.js
+++ source/js/script.js
@@ -15,12 +15,12 @@ navToggle.addEventListener('click', function() {
 
 var map;
 
-function initMap() {
+function initMap(desktopSize) {
   var coordinates = {lat: 59.938851, lng: 30.323047},
       center = coordinates,
       image = '../img/map-pin.png';
 
-  if (window.innerWidth >= 1300) {
+  if (desktopSize) {
     center = {lat: 59.939100, lng: 30.319379};
   }
 
@@ -35,5 +35,14 @@ function initMap() {
   });
 }
 
-initMap();
+var desktopSize = (window.innerWidth >= 1300);
+window.addEventListener("resize", function(){
+  if (desktopSize != (window.innerWidth >= 1300)) {
+    desktopSize = (window.innerWidth >= 1300);
+    initMap(desktopSize);
+  }
+}, false);
+
+initMap(desktopSize);
+
 svg4everybody();
