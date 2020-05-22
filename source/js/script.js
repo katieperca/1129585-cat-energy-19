c6a17bdbd54df0f841e18b50a5bf57eeb6f2b585
katieperca <katieperca@gmail.com>
2020-05-22 12:45:28 +0300

    Исправляет часть проблем перед предзащитой

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,28 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+var map;
+
+function initMap() {
+  var coordinates = {lat: 59.938851, lng: 30.323047},
+      center = coordinates,
+      image = '../img/map-pin.png';
+
+  if (window.innerWidth >= 1300) {
+    center = {lat: 59.939100, lng: 30.319379};
+  }
+
+  map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        zoom: 17
+  });
+  marker = new google.maps.Marker({
+      position: coordinates,
+      map: map,
+      icon: image
+  });
+}
+
+initMap();
+svg4everybody();
